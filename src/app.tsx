75e97ca43e2c3d17d9204df67c5325d859a1e7a9
Pedrowesley <pedrodev101@gmail.com>
2024-02-12 10:31:28 -0300

    feat(notes): add note creation functionality and persistent storage

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,33 @@
-import logo from "./assets/logo-nlw-expert.svg";
-import { NewNoteCard } from "./components/new-note-card";
-import { NoteCard } from "./components/note-card";
+import { useEffect, useState } from 'react';
+import logo from './assets/logo-nlw-expert.svg';
+import { NewNoteCard } from './components/new-note-card';
+import { NoteCard, NoteCardProps } from './components/note-card';
 
 export function App() {
+  const [notes, setNotes] = useState<NoteCardProps[]>([]);
+
+  const LOCAL_STORAGE_KEY = 'notes';
+
+  useEffect(() => {
+    const storedNotes = localStorage.getItem(LOCAL_STORAGE_KEY);
+    if (storedNotes) {
+      setNotes(JSON.parse(storedNotes));
+    }
+  }, []);
+
+  function onNoteCreated(content: string) {
+    const newNote = {
+      note: {
+        id: crypto.randomUUID(),
+        date: new Date(),
+        content,
+      },
+    };
+
+    const updatedNotes = [newNote, ...notes];
+    setNotes(updatedNotes);
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(updatedNotes));
+  }
   return (
     <div className="mx-auto max-w-6xl my-12 space-y-6">
       <img src={logo} alt="NLW Expert" />
@@ -18,21 +43,11 @@ export function App() {
       <div className="h-px bg-slate-700" />
 
       <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
-        <NewNoteCard />
-        <NoteCard
-          note={{
-            id: "1",
-            date: new Date(),
-            content: "Minha primeira nota",
-          }}
-        />
-        <NoteCard
-          note={{ id: "2", date: new Date(), content: "Minha segunda nota" }}
-        />
+        <NewNoteCard onCreate={onNoteCreated} />
 
-        <NoteCard
-          note={{ id: "3", date: new Date(), content: "Minha terceira nota" }}
-        />
+        {notes.map((note) => (
+          <NoteCard key={note.note.id} note={note.note} />
+        ))}
       </div>
     </div>
   );
